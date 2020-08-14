1bd5264c5a43015ac9e5ed53ccf4c79f5e94b730
David Gagnon <toogreen@15inch.lan>
2020-08-14 17:58:18 -0400

    added -static in the path for caricatures

--- src/components/CaricaturesDb.js
+++ src/components/CaricaturesDb.js
@@ -1,15 +1,15 @@
 const CaricaturesDb = [
 	{
 		id: 1,
-		img: "http://toogreen.ca/china/img/caricature.jpg"
+		img: "http://toogreen.ca/china-static/img/caricature.jpg"
 	},
 	{
 		id: 2,
-		img: "http://toogreen.ca/china/img/caricature2.jpg"
+		img: "http://toogreen.ca/china-static/img/caricature2.jpg"
 	},
 	{
 		id: 3,
-		img: "http://toogreen.ca/china/img/caricature3.jpg"
+		img: "http://toogreen.ca/china-static/img/caricature3.jpg"
 	}
 ]
 
