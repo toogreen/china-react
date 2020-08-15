3c840e57b1478d2372734dc220c492d417e2ea61
David Gagnon <toogreen@gmail.com>
2020-08-14 21:14:44 -0400

    added the no-mobile class to the ad so that it won't show up on mobile

--- src/components/Ad.js
+++ src/components/Ad.js
@@ -3,7 +3,7 @@ import React from "react"
 function Ad(){
 	return (
 		<div className="ad-container">
-			<iframe title="a-ads" data-aa="1415756" src="//ad.a-ads.com/1415756?size=468x60" scrolling="no" className="ad" allowtransparency="true"></iframe>
+			<iframe title="a-ads" data-aa="1415756" src="//ad.a-ads.com/1415756?size=468x60" scrolling="no" className="ad no-mobile" allowtransparency="true"></iframe>
 		</div>
 	)
 }
